c951fda057413f064e714548ca57392f2e8fd3d7
jeeknowme <cyrusloniego2@gmail.com>
2023-06-17 01:19:24 +0800

    completed crud functionalities

---
+++ components/Nav.jsx
@@ -0,0 +1,135 @@
+"use client";
+
+import { getProviders, signIn, signOut, useSession } from "next-auth/react";
+import Image from "next/image";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
+const Nav = () => {
+  const { data: session } = useSession();
+  const [providers, setProviders] = useState(null);
+  const [toggleDropDown, setToggleDropDown] = useState(false);
+
+  useEffect(() => {
+    const setProvider = async () => {
+      const response = await getProviders();
+      setProviders(response);
+    };
+    setProvider();
+  }, []);
+
+  return (
+    <nav className="flex-between w-full mb-16 pt-3">
+      <Link href="/" className="flex gap-2 flex-center">
+        <Image
+          width={30}
+          height={30}
+          className="object-contain"
+          src="/assets/images/logo.svg"
+          alt="logo"
+        />
+        <p className="logo_text ml-1">Promptopia</p>
+      </Link>
+
+      {/* Desktop Navigation */}
+      {/* ONly shows from small device and UP */}
+      <div className="sm:flex hidden">
+        {session?.user ? (
+          <div className="flex gap-3 md:gap-5">
+            <Link href="/create-prompt" className="black_btn">
+              Create Post
+            </Link>
+            <button onClick={signOut} className="outline_btn" type="button">
+              Sign Out
+            </button>
+            <Link href="/profile">
+              <Image
+                alt="profile"
+                src={session?.user.image}
+                className="rounded-full"
+                width={37}
+                height={37}
+              />
+            </Link>
+          </div>
+        ) : (
+          <>
+            {providers &&
+              Object.values(providers).map((provider) => {
+                return (
+                  <button
+                    onClick={() => signIn(provider.id)}
+                    type="button"
+                    key={provider.name}
+                    className="black_btn"
+                  >
+                    Sign In
+                  </button>
+                );
+              })}
+          </>
+        )}
+      </div>
+
+      {/* Mobile Navigation */}
+      <div className="sm:hidden flex relative">
+        {session?.user ? (
+          <div className="flex">
+            <Image
+              alt="profile"
+              src={session?.user.image}
+              className="rounded-full"
+              width={37}
+              height={37}
+              onClick={() => setToggleDropDown((prev) => !prev)}
+            />
+            {toggleDropDown && (
+              <div className="dropdown">
+                <Link
+                  href="/profile"
+                  className="dropdown_link"
+                  onClick={() => setToggleDropDown(false)}
+                >
+                  My Profile
+                </Link>
+                <Link
+                  href="/create-prompt"
+                  className="dropdown_link"
+                  onClick={() => setToggleDropDown(false)}
+                >
+                  Create Prompt
+                </Link>
+                <button
+                  type="button"
+                  className="mt-5 w-full black_btn"
+                  onClick={() => {
+                    setToggleDropDown(false);
+                    // signOut();
+                  }}
+                >
+                  Sign Out
+                </button>
+              </div>
+            )}
+          </div>
+        ) : (
+          <>
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <button
+                  onClick={() => signIn(provider.id)}
+                  type="button"
+                  key={provider.name}
+                  className="black_btn"
+                >
+                  Sign In
+                </button>
+              ))}
+          </>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
